ca730c0fbc4094cedd17d31440ccdbed21d13a00
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-15 23:25:42 +0200

    Add tests for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../model/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-  Todo.deleteMany({}).then(() => done());
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
-describe('Post /todos', () => {
+describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
 
@@ -24,7 +32,7 @@ describe('Post /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +51,23 @@ describe('Post /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+        expect(res.body.todos[0].text).toBe(todos[0].text);
+        expect(res.body.todos[1].text).toBe(todos[1].text);
+      })
+      .end(done);
   });
 });
