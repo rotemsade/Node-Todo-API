91695ff109525dfee5c18a0a2d72ecc0fc689aea
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-16 01:04:19 +0200

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/model/todo');
+const {User} = require('./../server/model/user');
+
+// var id = '5bede404d2ff680fb2be926111';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+var id = '5bed1777c86974551b88a6ce';
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('User not found');
+  }
+  console.log('User By Id', user);
+}).catch((e) => console.log(e));
