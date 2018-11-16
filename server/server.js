0e39faf84294f5aad3f381c5625542b162d4dbf9
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-16 10:00:17 +0200

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./model/todo');
 var {User} = require('./model/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -41,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
@@ -49,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
