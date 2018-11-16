7e9f6be9793ee44e61665b0f6a43f5cf6814ee5d
Rotem Sade <rotemsade@rotems-mbp.mynet>
2018-11-16 17:23:58 +0200

    Add test cases for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,41 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  var hexId = todos[1]._id.toHexString();
+  it('should delete a todo', (done) => {
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBeNull();
+          done();
+        }).catch((e) => done());
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should retrun 404 if object id is invalid', (done) => {
+    var hexId = new ObjectID().toHexString() + '12';
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+});
