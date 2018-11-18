526b8333af01f463de92ca424e84ba7fc46d430d
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-18 23:15:45 +0200

    Update test suite to match Expect v.23

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -128,7 +128,7 @@ describe('DELETE /todos/:id', () => {
         }
 
         Todo.findById(hexId).then((todo) => {
-          expect(todo).toBeNull();
+          expect(todo).toBeFalsy();
           done();
         }).catch((e) => done());
       });
@@ -147,7 +147,7 @@ describe('DELETE /todos/:id', () => {
         }
 
         Todo.findById(hexId).then((todo) => {
-          expect(todo).not.toBeNull();
+          expect(todo).toBeTruthy();
           done();
         }).catch((e) => done());
       });
@@ -188,7 +188,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(body.text);
         expect(res.body.todo.completed).toBe(true);
-        expect(res.body.todo.completedAt).not.toBeNull();
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done);
   });
@@ -223,7 +223,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(body.text);
         expect(res.body.todo.completed).toBe(false);
-        expect(res.body.todo.completedAt).toBeNull();
+        expect(res.body.todo.completedAt).toBeFalsy();
       })
       .end(done);
   });
@@ -263,8 +263,8 @@ describe('POST /users', () => {
       .send({email, password})
       .expect(200)
       .expect((res) => {
-        expect(res.headers['x-auth']).not.toBeNull();
-        expect(res.body._id).not.toBeNull();
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
       .end((err) => {
@@ -273,7 +273,7 @@ describe('POST /users', () => {
         }
 
         User.findOne({email}).then((user) => {
-          expect(user).not.toBeNull();
+          expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
         }).catch((e) => done(e));
@@ -313,7 +313,7 @@ describe('POST /users/login', () => {
       })
       .expect(200)
       .expect((res) => {
-        expect(res.headers['x-auth']).not.toBeNull();
+        expect(res.headers['x-auth']).toBeTruthy();
       })
       .end((err, res) => {
         if (err) {
@@ -321,7 +321,7 @@ describe('POST /users/login', () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[1]).toMatchObject({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: 'auth',
             token: res.headers['x-auth']
           });
@@ -339,7 +339,7 @@ describe('POST /users/login', () => {
       })
       .expect(401)
       .expect((res) => {
-        expect(res.headers['x-auth']).toBe(undefined);
+        expect(res.headers['x-auth']).toBeFalsy();
       })
       .end((err, res) => {
         if (err) {
@@ -362,7 +362,7 @@ describe('DELETE /users/me/token', () => {
       .send()
       .expect(200)
       .expect((res) => {
-        expect(res.headers['x-auth']).toBe(undefined);
+        expect(res.headers['x-auth']).toBeFalsy();
       })
       .end((err, res) => {
         if (err) {
