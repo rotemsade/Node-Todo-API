d14dd6f92b50aca4c0efd775cbb5af3ad91be302
Rotem Sade <rotemsade71@gmail.com>
2020-03-07 02:04:13 +0200

    Packages updates & set  useUnifiedTopology

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,40 +1,44 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require("mongodb");
 
 // var obj = new ObjectID();
 // console.log(obj);
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true }, (err, client) => {
-  if (err) {
-    return console.log('Unable to connect to MongoDB server');
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
+    }
+
+    console.log("Connected to MongoDB server");
+    const db = client.db("TodoApp");
+
+    // db.collection('Todos').insertOne({
+    //   text: 'Somthing to do',
+    //   completed: false
+    // }, (err, result) => {
+    //   if (err) {
+    //     return console.log('Unable to insert todo', err);
+    //   };
+    //
+    //   console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //   name: 'Rotem Sade',
+    //   age: 47,
+    //   location: 'Israel'
+    // }, (err, result) => {
+    //   if (err) {
+    //     return console.log('Unable to insert a user', err);
+    //   };
+    //
+    //   //console.log(JSON.stringify(result.ops, undefined, 2));
+    //   console.log(result.ops[0]._id.getTimestamp());
+    // });
+
+    client.close();
   }
-
-  console.log('Connected to MongoDB server');
-  const db = client.db('TodoApp');
-
-  // db.collection('Todos').insertOne({
-  //   text: 'Somthing to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert todo', err);
-  //   };
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
-
-  // db.collection('Users').insertOne({
-  //   name: 'Rotem Sade',
-  //   age: 47,
-  //   location: 'Israel'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert a user', err);
-  //   };
-  //
-  //   //console.log(JSON.stringify(result.ops, undefined, 2));
-  //   console.log(result.ops[0]._id.getTimestamp());
-  // });
-
-  client.close();
-});
+);
